622c1651a636eefe5e79132e1400c2294ea3c5fd
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-09 17:58:04 +0530

    Added Bootstrap

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,20 @@ import app from './app'
 import Constants from './Constants'
 import chalk from "chalk"
 import morgan from "morgan"
+import path from "path"
+import express from "express"
 
 app.use(morgan('tiny'));
+app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
+app.use('/css', express.static(path.join(__dirname, '/Public/css')))
+
+app.set('views', path.join(__dirname, '/Views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.send("Hello from Node Almanac");
+    res.render('Landing');
 });
+
 
 app.listen(Constants.PORT, function(){
     console.log(`listening on Port ${chalk.blue('' + Constants.PORT)}`);
