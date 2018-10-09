e51c9ab36e0760872fd87e88d6cdc1c6dcd7d1b8
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-09 18:19:14 +0530

    Added code refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import * as bodyParser from "body-parser";
+import path from "path";
+import morgan from "morgan";
+
 
 class App {
     public app: express.Application;
@@ -12,8 +15,20 @@ class App {
     private config(): void{
         // support application/json type post data
         this.app.use(bodyParser.json());
+
         //support application/x-www-form-urlencoded post data
         this.app.use(bodyParser.urlencoded({ extended: false }));
+        
+        // For logging request data automatically
+        this.app.use(morgan('tiny'));
+
+        // Add path for static css files
+        this.app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
+        this.app.use('/css', express.static(path.join(__dirname, '/Public/css')))
+        
+        // Add view path and view engine
+        this.app.set('views', path.join(__dirname, '/Views'));
+        this.app.set('view engine', 'ejs');
     }
 }
 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
 import app from './app'
 import Constants from './Constants'
 import chalk from "chalk"
-import morgan from "morgan"
-import path from "path"
-import express from "express"
 
-app.use(morgan('tiny'));
-app.use('/css', express.static(path.join(__dirname, '../node_modules/bootstrap/dist/css')))
-app.use('/css', express.static(path.join(__dirname, '/Public/css')))
-
-app.set('views', path.join(__dirname, '/Views'));
-app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
     res.render('Landing');
